c647883e50e4cdb12bbde7825843b58854ebed0a
Yos Riady <1084226+yosriady@users.noreply.github.com>
2024-12-09 10:01:29 +0700

    gitignore .env, update to latest sdk (#1)

--- src/App.js
+++ src/App.js
@@ -3,11 +3,10 @@ import { FormoAnalyticsProvider } from '@formo/analytics';
 import MainPage from './components/MainPage';
 
 const API_KEY = process.env.REACT_APP_FORMO_ANALYTICS_API_KEY;
-const PROJECT_ID = process.env.REACT_APP_FORMO_ANALYTICS_PROJECT_ID;
 
 function App() {
   return (
-    <FormoAnalyticsProvider apiKey={API_KEY} projectId={PROJECT_ID}>
+    <FormoAnalyticsProvider apiKey={API_KEY} options={{}}>
       <MainPage />
     </FormoAnalyticsProvider>
   );
