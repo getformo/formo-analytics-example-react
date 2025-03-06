6548d38319ecd2d9ab7e9b6bfb801b41d597fcb3
Huynh Thanh Tam <huynhthanhtam2605@gmail.com>
2025-03-06 14:19:55 +0700

    Update formo analytics

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import './App.css';
-import { FormoAnalyticsProvider } from '@formo/analytics';
-import MainPage from './components/MainPage';
+import "./App.css";
+import { FormoAnalyticsProvider } from "@formo/analytics";
+import MainPage from "./components/MainPage";
 
-const API_KEY = process.env.REACT_APP_FORMO_ANALYTICS_API_KEY;
+const WRITE_KEY = process.env.REACT_APP_FORMO_ANALYTICS_WRITE_KEY;
 
 function App() {
   return (
-    <FormoAnalyticsProvider apiKey={API_KEY} options={{}}>
+    <FormoAnalyticsProvider writeKey={WRITE_KEY} options={{}}>
       <MainPage />
     </FormoAnalyticsProvider>
   );
