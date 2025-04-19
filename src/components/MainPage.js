e8328c077121953771449af55e88c3846fae0169
Huỳnh Thanh Tâm <55596752+TamHuynhTee@users.noreply.github.com>
2025-04-19 22:52:39 +0700

    Update formo analytics (#4)

--- src/components/MainPage.js
+++ src/components/MainPage.js
@@ -1,26 +1,26 @@
-import { useFormoAnalytics } from '@formo/analytics';
-import logo from '../logo.svg';
+import { useFormo } from "@formo/analytics";
+import logo from "../logo.svg";
 
 function MainPage() {
-  const analytics = useFormoAnalytics();
+  const analytics = useFormo();
 
   return (
-    <div className='App'>
-      <header className='App-header'>
+    <div className="App">
+      <header className="App-header">
         <button
-          onClick={() => analytics.track('custom_event', { key: 'value' })}
+          onClick={() => analytics.track("custom_event", { key: "value" })}
         >
           Click here to track click button event
         </button>
-        <img src={logo} className='App-logo' alt='logo' />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className='App-link'
-          href='https://reactjs.org'
-          target='_blank'
-          rel='noopener noreferrer'
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Learn React
         </a>
